e28d2ee859cf7eb884585b7878c414ae23691de8
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-17 23:27:02 +0530

    Create codekata4.c

---
+++ codekata4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	char c;
+	
+	scanf("%c",&c);
+	
+	if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') 
+	printf("Alphabet");
+	else
+	printf("No");
+	
+	return 0 ;
+}
