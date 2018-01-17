ccc9f053211028447e41471d87fa228a3408b829
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-17 22:55:04 +0530

    Update codecata1.c

--- codecata1.c
+++ codecata1.c
@@ -1,1 +1,17 @@
-cvcvbcvc
+#include<stdio.h>
+int main(){
+	
+	int a;
+	
+	scanf("%d",&a);
+	
+	if(a>0 && a<100000)
+	printf("Positive");
+	else if (a<0 && a> -100000)
+	printf("Negative");
+	
+	else
+	printf("Zero");
+
+	return 0;
+}
