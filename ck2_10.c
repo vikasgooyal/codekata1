15367afbc78b09e19ece1c398ee031531e5f5270
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-18 22:52:00 +0530

    Create ck2_10.c

---
+++ ck2_10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int m,n,i,p,f;
+	
+	scanf("%d",&n ,&m);
+	
+	if(n<0)
+	printf("No");
+	
+	else
+	{
+	
+	for(i=1 ; i <= n ; i++)
+{
+		p = n* i;
+printf("%d\t",p);
+
+}
+
+}
+}
+
