cbe8618a1b193fdc5f87abf8a1349d59011c5f43
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-18 19:53:15 +0530

    Create ck2_5.c

---
+++ ck2_5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int m,n,i,p,f;
+	
+	scanf("%d %d",&n ,&m);
+	
+	if(n<0)
+	printf("Invalid output");
+	
+	for(i=n+1 ; i < m ; i++)
+	{
+	
+	if(i%2 == 0)
+	{
+		printf("%d\t",i);
+		
+}
+		}
+
+}
+
+   
